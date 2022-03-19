fc368fd4971a8c2b1e6644bdffd9781ad93af6dd
Haotian Mo <toreimimo@gmail.com>
2022-03-18 22:42:51 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 direction;
+    public float gravity = -9.8f;
+
+    // Called every frame, usually handles inputs
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
